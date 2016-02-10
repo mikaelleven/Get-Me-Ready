02fbb50a42813a52561954a3b062a627b9377872
Mikael Levén <mleven@sto-milemac13.co.int>
2016-02-10 14:47:43 +0100

    Added Powertools

---
+++ Mac/Powertools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing power tools, please wait..."
+echo 
+
+# Note: We always honour Apple Store apps..
+
+# AppCleaner
+#brew cask install appcleaner
+
+# Disk Cleaner X
+#brew cask install disk-inventory-x
