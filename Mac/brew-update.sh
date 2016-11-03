9a8adfc58f7612af991174426498a58753556e1c
Mikael Levén <mikael.leven@gmail.com>
2016-11-03 08:58:30 +0100

    Mac: Added caskupdate to brew update script + minor log tweak

--- Mac/brew-update.sh
+++ Mac/brew-update.sh
@@ -3,6 +3,9 @@
 brew=/usr/local/bin/brew
 logger=/usr/bin/logger
 
+echo -ne Updating Homebrew... 
 $brew update 2>&1  | $logger -t brewup.update
+$brew cu 2>&1  | $logger -t brewup.update
 $brew upgrade 2>&1 | $logger -t brewup.upgrade
 $brew cleanup 2>&1 | $logger -t brewup.cleanup
+echo "Done! Run '$ cat /var/log/system.log | grep brewup' to view log entries"
