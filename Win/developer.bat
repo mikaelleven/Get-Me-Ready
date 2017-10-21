3bb8b91241b010be83397aa6ba26b8bd6bf6a0be
Mikael Levén <mikaelleven@users.noreply.github.com>
2017-10-21 18:12:59 +0200

    Win: Major overhaul of the Developer pack
    Removed lot's of faulty or outdated installs.
    Added some missing must-have apps.
    Overall cleanup.

--- Win/developer.bat
+++ Win/developer.bat
@@ -1,88 +1,96 @@
 
-::choco install DotNet4.5
-choco install webpi
-
 
 ECHO "Installing developer tools..."
 
 
 REM ### LANGUAGES / FRAMEWORKS ###
-REM choco install ruby
-REM choco install python
-REM  choco install python3
-REM choco install php
+cinst DotNet4.5.2
+cinst DotNet4.6.1
+::cinst DotNet4.7
 
-REM NodeJS
-choco install nodejs.install
-choco install nodist
-::start "https://github.com/coreybutler/nvm-windows#node-version-manager-nvm-for-windows"
+cinst dotnetcore
+cinst dotnetcore-sdk
+
+::cinst ruby
+cinst python REM python3
+::cinst python3
+cinst python2
+::cinst pip
+::cinst php
+
+REM ### NodeJS ###
+::cinst nodejs.install
+::cinst nodist
+cinst nvm
+nvm install latest
 
 
 REM ### APPLICATION FRAMEWORKS	###
-choco install electron
+cinst electron
 
 REM ### DATABASES / STORES / CACHE ###
-
-REM choco install rabbitmq
-REM choco install redis
-REM choco install redis-64
-REM choco install memcached
-
-REM Databases
-choco install mongodb
-choco install postgresql
+::cinst rabbitmq
+::cinst redis
+::cinst redis-64
+::cinst memcached
+::cinst mongodb
+::cinst postgresql
 
 REM SQLite
-choco install SQLite
-choco install sqliteadmin
-REM choco install sqlitebrowser
+::cinst SQLite
+::cinst sqliteadmin
+::cinst sqlitebrowser
 
 REM SQL Server
-choco install mssql2014express-defaultinstance
-choco install mssqlservermanagementstudio2014express
-REM choco install sqlserverlocaldb
-REM choco install MsSqlServer2012Express
+cinst MsSqlServer2014Express
+::cinst mssql2014express-defaultinstance
+cinst mssqlservermanagementstudio2014express
+::cinst sqlserverlocaldb
+::cinst MsSqlServer2012Express
 
 
-REM ### DEVELOPER TOOLS ###
-
-choco install webpi
-choco install ilmerge
-REM choco install fiddler
-choco install fiddler4
-choco install dotPeek
-
-REM IDEs / Editors
-choco install visualstudiocode
-::choco install visualstudio2015enterprise
-::choco install visualstudio2015professional
-::choco install visualstudio2015community
+REM ### IDE:S / EDITORS ###
+::cinst visualstudio2015enterprise
+::cinst visualstudio2015professional
+::cinst visualstudio2015community
+cinst VisualStudio2017Community
+::cinst VisualStudio2017Professional
+cinst visualstudiocode
 
 
-REM Git clients
-choco install sourcetree
-choco install github
-::choco install githubforwindows
-choco install gitkraken
-choco install kdiff3
+REM ### TOOLS ###
+cinst webpi
+cinst ilmerge
+::cinst fiddler
+cinst fiddler4
+cinst dotPeek
+cinst kdiff3
+cinst nuget.commandline
 
 
-REM Cloud services
-::choco install awstools.powershell
-::choco install awscli
-::choco install windowsazurepowershell
+REM ### SOURCE CONTROL ###
+cinst sourcetree
+cinst github
+::cinst githubforwindows
+cinst gitkraken
 
 
->> choco install VisualStudio2013Ultimate
-cinst VisualStudio2013Ultimate -InstallArguments "/Features:'WebTools Win8SDK' /ProductKey:AB1CD-EF2GH-IJ3KL-MN4OP-QR5ST"
-You simply need to send a white space delimited list of the features to install via the InstallArguments parameter. Here are the available features:
-Blend
-LightSwitch
-VC_MFC_Libraries
-OfficeDeveloperTools
-SQL
-WebTools
-Win8SDK
-SilverLight_Developer_Kit
-WindowsPhone80
->>>>>>>>
+REM ### CLOUD SERVICES ###
+::cinst awstools.powershell
+::cinst awscli
+cinst WindowsAzurePowerShell
+
+
+rem >> cinst VisualStudio2013Ultimate
+rem cinst VisualStudio2013Ultimate -InstallArguments "/Features:'WebTools Win8SDK' /ProductKey:AB1CD-EF2GH-IJ3KL-MN4OP-QR5ST"
+rem You simply need to send a white space delimited list of the features to install via the InstallArguments parameter. Here are the available features:
+rem Blend
+rem LightSwitch
+rem VC_MFC_Libraries
+rem OfficeDeveloperTools
+rem SQL
+rem WebTools
+rem Win8SDK
+rem SilverLight_Developer_Kit
+rem WindowsPhone80
+rem >>>>>>>>
