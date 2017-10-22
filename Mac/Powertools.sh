d7dfce8e52834110c14525c9063ff71890dc4070
Mikael Levén <mikaelleven@users.noreply.github.com>
2017-10-22 17:41:14 +0200

    Mac: Updated FreeFileSync to v9.3

--- Mac/Powertools.sh
+++ Mac/Powertools.sh
@@ -21,8 +21,7 @@ brew cask install freefilesync
 # -- 8.5 issue fix >>>>
 if ! [ -e /Applications/FreeFileSync.app ] 
 then
-	echo "here"
-	curl -L -A 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0' -H 'Referer: http://www.freefilesync.org/archive.php' http://www.freefilesync.org/archive/FreeFileSync_8.4_Mac_OS_X.zip >> ~/Library/Caches/Homebrew/Cask/freefilesync--8.4.zip
+	curl -L -A 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0' -H 'Referer: https://www.freefilesync.org/archive.php' https://www.freefilesync.org/download/FreeFileSync_9.3_macOS.zip >> ~/Library/Caches/Homebrew/Cask/freefilesync--9.3.zip
 	brew cask install freefilesync
 
 	# If still not ok, let's open a browser to manully download
